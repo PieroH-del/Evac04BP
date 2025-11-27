c8db35b3f615ab36a590b352ecd4dcaed1bba0b0
PieroH-del <pharryson1@alumno.uned.es>
2025-11-27 17:20:03 -0500

    Initial commit - BataPeru project

---
+++ src/main/java/com/example/BataPeru/BataPeruApplication.java
@@ -0,0 +1,13 @@
+package com.example.BataPeru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BataPeruApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BataPeruApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BataPeru/BataPeruApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BataPeru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BataPeruApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
